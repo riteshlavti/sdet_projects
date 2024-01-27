c9bb7a9f6adcda6b6f1fc4634239ca8a2ac36485
Ritesh Lavti <ritesh.lavti@intimetec.com>
2024-01-27 21:54:46 +0530

    programming practice

---
+++ BasicProgrammingPractice/interpolation.cs
@@ -0,0 +1,19 @@
+ 
+using System;
+using System.Text;
+ 
+namespace Program4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            //string str = $"{2 + 2}, {5 + 5}";
+            string str= """ Hello "hii" """;
+ 
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
