068c01c4f44747b4a0134ca9c4ded8dc36707660
Ritesh Lavti <ritesh.lavti@intimetec.com>
2024-01-31 17:20:11 +0530

    Date and time project

---
+++ DateAndTimeProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace DateAndTimeProject
+{
+    class DateAndTimePractice
+    {
+        static void Main()
+        {
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine(currentDateTime);
+
+            DateTime futureDateTime = currentDateTime.AddHours(3);
+            Console.WriteLine(futureDateTime);
+
+            string formattedDate = currentDateTime.ToString("yyyy/dd-MM");
+            Console.WriteLine(formattedDate);
+
+            TimeSpan timeDifference = futureDateTime - currentDateTime;
+            Console.WriteLine(timeDifference);
+
+            string shortDate = currentDateTime.ToShortDateString();
+            Console.WriteLine(shortDate);
+        }
+    }
+}
