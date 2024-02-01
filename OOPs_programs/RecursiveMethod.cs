9f3d13a317df43adb535a55da1861ba9cae23c2f
Ritesh Lavti <ritesh.lavti@intimetec.com>
2024-02-01 19:41:57 +0530

    added new programs of regex and recursion

---
+++ OOPs_programs/RecursiveMethod.cs
@@ -0,0 +1,26 @@
+using System;
+namespace RecursiveMethod
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a number to calculate its factorial:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            long factorial = CalculateFactorial(number);
+            Console.WriteLine("Factorial of {0} is: {1}", number, factorial);
+        }
+        static long CalculateFactorial(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * CalculateFactorial(n - 1);
+            }
+        }
+    }
+}
---
+++ OOPs_programs/RegExProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace RegExPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+            Console.WriteLine("Enter an email address:");
+            string email = Console.ReadLine();
+
+            if (Regex.IsMatch(email, emailPattern))
+            {
+                Console.WriteLine("Valid email address.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid email address.");
+            }
+        }
+    }
+
+}
